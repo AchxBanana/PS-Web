f41d565acfd19e29515831788c7cfe9322d705df
DEBUGASFXCK <achxpj@gmail.com>
2024-06-01 20:25:04 +0700

    Pong has spin right now

--- src/components/animateonLoad.jsx
+++ src/components/animateonLoad.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function AnimationsOnLoad() {
+  // useEffect(() => {
+  //   const handlePageReload = () => {
+  //     const element = document.getElementById('pong');
+  //     if (element) {
+  //       element.style.transform = 'translate(2000px, 0px)';
+  //     }
+  //   };
+  //
+  //   window.addEventListener('beforeunload', handlePageReload);
+  //
+  //   return () => {
+  //     window.removeEventListener('beforeunload', handlePageReload);
+  //   };
+  // }, []);
+
   return (
-    <div className="flex justify-center">
+    <div>
       <img
         src="pong.png"
         alt="Pong"
-        className="animate-spin h-96"
-        style={{
-          transform: "rotate(0deg)",
-          transition: "transform 1s linear",
-        }}
+        className="h-[500px] animated-element"
+        id="pong"
       />
     </div>
   );

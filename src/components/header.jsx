3aee970640aab56a0c0affa6349fa5d639688fb0
AchiBanana <achxpj@gmail.com>
2024-06-03 12:43:05 +0700

    Change hero

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,20 @@
 const Header = () => {
-  return(
-    <div className="flex">
-      <div className="ml-20 mt-10">
-        <h1 className="text-6xl text-blue-600 font-kola animated-font">Welcome TO PS</h1>
-        <h1 className="text-2xl text-pink-500 font-cus animated-font">Possibility Shit School</h1>
+  return (
+    <>
+      <div className="flex justify-between ml-10 mr-10">
+        <div className=" mt-10 ">
+          <h1 className="text-6xl text-blue-600 font-kola animated-font">Welcome TO PS</h1>
+          <h1 className="text-2xl text-pink-500 font-cus animated-font">Possibility Shit School</h1>
+        </div>
+        <div>
+          <img
+            src="pong.png"
+            className="h-[880px]  animated-element"
+            id="pong"
+          />
+        </div>
       </div>
-      <div className="ml-2 p-2">
-        <img
-          src="pong.png"
-          className="h-48 animated-element ml-[210%] md:h-auto md:ml-[100%] lg:h-[830px] lg:ml-[230%]"
-          id="pong"
-        />
-      </div>
-      <div className="text-center mx-auto mt-10">
-        <h1 className="text-gray-200 text-5xl font-bold mb-[210px]">Potisarn School</h1>
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,14 +3,14 @@ import { PiChalkboardTeacherBold } from "react-icons/pi";
 import { SiGoogleclassroom } from "react-icons/si";
 import { FaBookOpenReader } from "react-icons/fa6";
 import { FaMoneyBillAlt } from "react-icons/fa";
-const Services = () =>{
-  return(
+const Services = () => {
+  return (
     <>
       <div className="flex justify-center gap-5 mt-10">
-        <a className="" href="#"><IoSchoolSharp color="white" className="h-12 w-12 p-3 bg-blue-500 rounded-full mb-4"/></a>
-        <a href="#"><PiChalkboardTeacherBold color="white" className="h-12 w-12 p-3 bg-pink-500 rounded-full mb-4"/></a>
-        <a href="#"><FaBookOpenReader color="white" className="h-12 w-12 p-3 bg-pink-500 rounded-full mb-4"/></a>
-        <a href="#"><FaMoneyBillAlt color="white" className="h-12 w-12 p-3 bg-pink-500 rounded-full mb-4"/></a>
+        <a className="" href="#"><IoSchoolSharp color="white" className="h-12 w-12 p-3 bg-blue-500 rounded-full mb-4" /></a>
+        <a href="#"><PiChalkboardTeacherBold color="white" className="h-12 w-12 p-3 bg-pink-500 rounded-full mb-4" /></a>
+        <a href="#"><FaBookOpenReader color="white" className="h-12 w-12 p-3 bg-orange-500 rounded-full mb-4" /></a>
+        <a href="#"><FaMoneyBillAlt color="white" className="h-12 w-12 p-3 bg-yellow-500 rounded-full mb-4" /></a>
       </div>
     </>
   )

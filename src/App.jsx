1f09c4fe672dd9e2cbf395e4a0d69b97386aa3c0
DEBUGASFXCK <achxpj@gmail.com>
2024-06-09 16:07:53 +0700

    Add pkg

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
-// import Navbar from './components/dropdowntest.jsx'
 
 import Header from './components/header.jsx'
 import Services from './components/services.jsx'
-import Footer from './components/footer.jsx'
 import Contant from './components/contant.jsx'
-
-const handleMouseMove = (event) => {
-  console.log('Mouse position:', event.clientX, event.clientY);
-  // Additional logic
-};
+import Footer from './components/Footer.jsx'
+import Imagegal from './components/imagegal.jsx'
 
 function App() {
-
   return (
     <>
-      <div className="bg-gradient-to-br from-zinc-900 to-gray-900">
-        <Navbar />
-        <Header />
-        <Services />
-        <Contant />
-      </div>
+        <div className="bg-gradient-to-tl from-blue-500 to-pink-600">
+          <Navbar />
+          <Header />
+          <Contant id="contant" />
+          <Imagegal />
+          <Footer />
+        </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
+import { ParallaxProvider } from "react-scroll-parallax";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

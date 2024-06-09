18a3510b07fa6a0571cc3d538cdc4f7b69009cf1
AchiBanana <achxpj@gmail.com>
2024-06-09 13:06:07 +0700

    Add something to make I've no space

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <div className="container mx-auto h-fit m-10 relative">
-        <img src="slide-18.jpg" className="absolute w-4/5 shadow-md shadow-gray-500 rounded-sm"/>
+        <img src="slide-18.jpg" className="absolute w-4/5 shadow-md shadow-gray-500 rounded-sm" />
         <div className="flex justify-between relative">
           <div className="mt-10 ml-10">
             <h1 className="text-2xl text-blue-600 font-mitr animated-font md:text-6xl">โพธิสารพิทยากร</h1>
@@ -22,6 +22,11 @@ const Header = () => {
               className="h-1/2 animated-element md:h-[760px] mr-10"
               id="pong"
             />
+          </div>
+        </div>
+        <div className="text-white text-3xl shadow-lg rounded-md h-32 flex font-mitr p-5 bg-blue-500">
+          <div className='flex'>
+            <h1 className="my-auto"> โพธิสาร คือสถานการศึกษา</h1>
           </div>
         </div>
       </div>
